859b6e6ba284e507d3694ad07deedcf32545440c
Diego M <diegaozims@hotmail.com.br>
2021-02-11 01:53:44 -0300

    Update ProcessamentoTemperatura.java

--- src/ProcessamentoTemperatura.java
+++ src/ProcessamentoTemperatura.java
@@ -1,7 +1,13 @@
-
+import java.util.Scanner;
 public class ProcessamentoTemperatura {
 
+	
+	
 	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		
 		// TODO Auto-generated method stub
 
 	}
