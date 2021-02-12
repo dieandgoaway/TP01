b150c4fabd4eb7761cca8ef4e7728d8126b64da2
Diego M <diegaozims@hotmail.com.br>
2021-02-12 03:09:40 -0300

    Update ProcessamentoTemperatura.java

--- src/ProcessamentoTemperatura.java
+++ src/ProcessamentoTemperatura.java
@@ -1,10 +1,23 @@
+/*
+ * Programa para processar médias de temperatura criado como Trabalho prático da disciplina de Orientação de Objetos em 12/02/2021
+ */
+
 import java.util.Scanner;
-import java.lang.reflect.Field;
+
 public class ProcessamentoTemperatura {	
+	/* Declaração de Array tridimensional de valores double para armazenar as temperaturas em cada uma das datas
+	 * com a terceira dimensão em aberto para ser ajustada de acordo com a quantidade de dias do referido mês
+	 */
 	public static double[][][] data = new double[12][10][];
+	// Flag para registrar o armazenamento de temperaturas (Valor padrão = falso)
+	public static boolean[][] armazenado = new boolean[12][10];
+	
+	//Método entradaTemperatura que registra as temperaturas na devida variável de mes e ano correspondente
 	public static void entradaTemperatura(){
-		//Declaração da quantidade de dias para cada array de data
-		data[0][0] = new double[31]; data[1][0] = new double[28]; data[2][0] = new double[31]; data[3][0] = new double[30];
+		/*Declaração da quantidade de dias para cada array de data corretamente ajustados para anos bissextos
+		 * Com a primeira variável do array responsável pelo mês(0 a 11) e a segunda pelo ano(0 a 9)
+		 */
+		 data[0][0] = new double[31]; data[1][0] = new double[28]; data[2][0] = new double[31]; data[3][0] = new double[30];
 		 data[4][0] = new double[31]; data[5][0] = new double[30]; data[6][0] = new double[31]; data[7][0] = new double[31]; 
 		 data[8][0] = new double[30]; data[9][0] = new double[31]; data[10][0] = new double[30]; data[11][0] = new double[31]; 
 		 data[0][1] = new double[31]; data[1][1] = new double[29]; data[2][1] = new double[31]; data[3][1] = new double[30]; 
@@ -38,7 +51,7 @@ public class ProcessamentoTemperatura {
 		int mesEntrada;
 		int anoEntrada;
 		Scanner ler = new Scanner(System.in);
-		System.out.println("Digite o mês que deseja cadastrar(1 a 12):");
+		System.out.println("Digite o mês que deseja cadastrar (1 a 12):");
 		mesEntrada = ler.nextInt();
 		System.out.println("Digite o ano de referência (2011 a 2020):");
 		anoEntrada = ler.nextInt();
@@ -50,15 +63,44 @@ public class ProcessamentoTemperatura {
 			System.out.println("Data Inválida");
 			return;
 		}
-		
+		//Loop para armazenar as temperaturas médias de cada dia do mês selecionado
 		for (int i = 0; i<(data[mesEntrada-1][anoEntrada-2011].length); i++){
 			System.out.println("Digite a média de temperatura do dia " + String.format("%02d", (i+1)) + "/" + String.format("%02d", mesEntrada) + "/" + anoEntrada);
 			data[mesEntrada-1][anoEntrada-2011][i] = ler.nextDouble();
 		}
-        
-		System.out.println("Dados do mês "+ String.format("%02d", mesEntrada) + "/" + anoEntrada + " cadastrados com sucesso!");
-		return;
+        //flag que indicará que o valor desse mes e ano foi armazenado
+		armazenado[mesEntrada-1][anoEntrada-2011] = true;
+		System.out.println("Dados do mês "+ String.format("%02d", mesEntrada) + "/" + anoEntrada + " cadastrados com sucesso!\n");
+		return;	
+	}
+	
+	public static void mediaTemperatura() {
+		int mesMedia;
+		int anoMedia;
+		double media = 0;
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite o mês do qual deseja a média (1 a 12):");
+		mesMedia = ler.nextInt();
+		System.out.println("Digite o ano de referência (2011 a 2020):");
+		anoMedia = ler.nextInt();
 		
+		if(mesMedia<1 || mesMedia>13) {
+			System.out.println("Data Inválida\n");
+			return;
+			}else if(anoMedia<2011 || anoMedia>2020) {
+			System.out.println("Data Inválida\n");
+			return;
+			}
+		if(!armazenado[mesMedia-1][anoMedia-2011]) {
+			System.out.println("A temperatura do mês " + String.format("%02d", mesMedia) + "/" + anoMedia + " ainda não foi cadastrada.\n");
+			return;
+		}
+		for (int j = 0; j<(data[mesMedia-1][anoMedia-2011].length); j++){
+			media += data[mesMedia-1][anoMedia-2011][j];
+		}
+		media /= data[mesMedia-1][anoMedia-2011].length;
+		System.out.println("A média de temperaturas do mês " + String.format("%02d", mesMedia) + "/" + anoMedia + " é :" + media + "\n");
+		return;
 		
 	}
 	
@@ -66,9 +108,11 @@ public class ProcessamentoTemperatura {
 		
 		
 		
-		int opcaoMenu, mes, ano;
+		int opcaoMenu;
 				
 		Scanner ler = new Scanner(System.in);
+		
+		while(true) {
 		System.out.println("1. Entrada das temperaturas");
 		System.out.println("2. Cálculo da temperatura média");
 		System.out.println("3. Cálculo da temperatura mínima");
@@ -80,12 +124,16 @@ public class ProcessamentoTemperatura {
 		opcaoMenu = ler.nextInt();
 		switch (opcaoMenu) {
 		case 1:
+			System.out.println("Opção 1. Entrada das temperaturas Selecionada\n");
 			ProcessamentoTemperatura.entradaTemperatura();
 			break;
 			
-		case 2: 
+		case 2:
+			System.out.println("Opção 2. Cálculo da temperatura média Selecionada\n");
+			ProcessamentoTemperatura.mediaTemperatura();
+			break;
 		}
-		
+		}
 		
 		
 		
